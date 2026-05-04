c1b7d27135a12545830f764d120d0462afff7919
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2026-05-04 20:52:52 -0300

    Fix negative dex and str artifact block

--- src/engine/process_effect_modifier_gain.rs
+++ src/engine/process_effect_modifier_gain.rs
@@ -19,9 +19,9 @@ pub fn process_effect_modifier_gain(
     }
 
     // Check artifact
-    // TODO: should block Sihpon Soul
-    if stacks > 0 && !modifier_def(kind).is_buff && modifier_has(modifiers, ModifierKind::Artifact)
-    {
+    let is_buff = modifier_def(kind).is_buff;
+    let is_debuff_attempt = (stacks > 0 && !is_buff) || (stacks < 0 && is_buff);
+    if is_debuff_attempt && modifier_has(modifiers, ModifierKind::Artifact) {
         let stacks_new = modifier_stacks(modifiers, ModifierKind::Artifact) - 1;
         if stacks_new < modifier_def(ModifierKind::Artifact).stacks_min {
             modifier_remove(modifiers, ModifierKind::Artifact);
