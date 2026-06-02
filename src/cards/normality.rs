6b5fdc8ab314852ccdab9c3515ba8f21f0d31c50
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2026-06-02 19:24:06 -0300

    Refactor curses

---
+++ src/cards/normality.rs
@@ -0,0 +1,26 @@
+use crate::entity::CardCostKind;
+use crate::entity::Entity;
+use crate::entity::PlayRestriction;
+use crate::entity::make_entity_card;
+use crate::types::CardColor;
+use crate::types::CardKind;
+use crate::types::CardName;
+use crate::types::CardRarity;
+
+pub static NORMALITY: Entity = make_entity_card(
+    CardName::Normality,
+    CardKind::Curse,
+    CardColor::Curse,
+    CardRarity::Curse,
+    0,
+    CardCostKind::Fixed,
+    false,
+    false,
+    false,
+    false,
+    false,
+    &[],
+    &[],
+    &[],
+    PlayRestriction::Never,
+);
